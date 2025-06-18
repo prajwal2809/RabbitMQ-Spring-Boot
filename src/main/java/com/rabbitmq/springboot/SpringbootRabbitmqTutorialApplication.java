e3d450343856e461b2de32e3176ee2314256bdb6
Prajwal Yemde <prajwalyemde@Prajwals-MacBook-Air.local>
2025-06-19 00:56:20 +0530

    rabbitMQ Implementation

---
+++ src/main/java/com/rabbitmq/springboot/SpringbootRabbitmqTutorialApplication.java
@@ -0,0 +1,16 @@
+package com.rabbitmq.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqTutorialApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootRabbitmqTutorialApplication.class, args);
+
+		
+	}
+
+}
